c02244d3a43e82b72d46be9d362dc0e760ac1105
Jordan Stanley Hall <hjordan6@zion.cs.byu.edu>
2019-03-14 21:26:25 -0600

    Initial commit

---
+++ app/src/main/java/com/hjordan6/familymapclient/Model/Model.java
@@ -0,0 +1,9 @@
+package com.hjordan6.familymapclient.Model;
+
+/**
+ * container for user info received from the server upon user login.
+ * Created by hjordan6 on 3/14/19.
+ */
+
+public class Model {
+}
---
+++ app/src/main/java/com/hjordan6/familymapclient/UI/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hjordan6.familymapclient.UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hjordan6.familymapclient.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
